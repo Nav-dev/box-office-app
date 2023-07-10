0f5fccc2e1900e6dc848f5697623daf3a9eb00ee
Nav-dev <sharmanav325@gmail.com>
2023-07-10 14:57:48 +0530

    search button added, made search dynamic and used tv-maze api for that

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+
 const Home = ()=> {
+    const [ searchStr, setSearchStr ] = useState("");
+
+    const onSearchOnInputChange = (ev) => {
+        setSearchStr(ev.target.value);
+    };
+
+    const onSearch =  async(ev) => {
+        ev.preventDefault();
+
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`
+        );
+        const body = await response.json();
+        console.log(body);
+    };
+
+    //https://api.tvmaze.com/search/shows?q=boys
+
     return (
          <div>
-            Home
-        </div>
-            );
+            <form onSubmit={onSearch}>
+              <input type="text" value={searchStr} onChange={onSearchOnInputChange} />
+              <button type="sumbit">Search</button>
+              </form>
+              </div>
+    );
 };
+              
 
 export default Home;

6d5d84a2e157594f9c52d0f508a8f7b17a3cd8fd
Nav-dev <sharmanav325@gmail.com>
2023-07-12 14:06:47 +0530

    installed react-query. replaced useShowId with UseQuery hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
+
+const queryClient = new QueryClient()
 import Show from './pages/Show';
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
@@ -31,6 +35,7 @@ function App() {
           <Route path="contact-us" element={<Contact />} />*/}
       </Routes>
     </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 

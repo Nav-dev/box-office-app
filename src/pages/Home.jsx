af0c3627ad3e566afff4013157590fe38a19ae8a
Nav-dev <sharmanav325@gmail.com>
2023-07-12 14:28:27 +0530

    used and added UseQuery on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { searchForShows, searchForPeople } from './../api/tvmaze';
 import SearchForm from '../components/SearchForm';
 import ShowGrid from '../components/shows/ShowGrid';
@@ -6,25 +7,18 @@ import ActorsGrid from '../components/actors/ActorsGrid';
 
 const Home = () => {
 
-  const [apiData, setApiData] = useState(null);
-  const [apiDataError, setApiDataError] = useState(null);
+  const [filter, setFilter] = useState('')
+  const { data: apiData, error:apiDataError } = useQuery({
+    queryKey: ['search', filter],
+    queryFn: () => filter.searchOption === 'shows' ? searchForShows(filter.q) : searchForPeople(filter.q),
+    // ⬇️ disabled as long as the filter is empty
+    enabled: !!filter,
+    refetchOnWindowFocus: false,
+});
+
 
   const onSearch = async ({ q, searchOption}) => {
-    try {
-      setApiDataError(null);
-      let result; 
-
-      if(searchOption === 'shows'){
-         result = await searchForShows(q);
-      setApiData(result);
-    } else{
-         result = await searchForPeople(q);
-         setApiData(result);
-    } 
-}
-    catch (error) {
-      setApiDataError(error);
-    }
+    setFilter({ q, searchOption })
   };
 
   const renderApiData = () => {

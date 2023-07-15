62877e51f33927e749693ca67f01df3a70c36626
Nav-dev <sharmanav325@gmail.com>
2023-07-15 13:59:20 +0530

    used usequery hook , fetch starred data

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -12,3 +12,9 @@ export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
 export const searchForPeople = query => apiGet(`/search/people?q=${query}`);
 
 export const getShowById = showId => apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`);
+
+export const getShowsByIds = async showIds => {
+    const promises = showIds.map(showId => apiGet(`/shows/${showId}`));
+
+   return Promise.all(promises);
+}

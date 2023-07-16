3300dacb0abe5e59bc4e707ed1cb9576501c5721
Nav-dev <sharmanav325@gmail.com>
2023-07-16 14:26:13 +0530

    installed styled components created global theme with sc. created custom radio used Navlink from react-router-dom to style active link

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import {QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
+import Show from './pages/Show';
+import { GlobalTheme } from './theme';
 
 const queryClient = new QueryClient()
-import Show from './pages/Show';
+
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Routes>
+      <GlobalTheme>
+      <BrowserRouter>
+        <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/Starred" element={<Starred />} />
@@ -20,21 +23,9 @@ function App() {
         <Route path="/show/:showId" element={<Show />} />
 
         <Route path="*" element={<div>Not found</div>} />
-        {/*<Route path="/" element={<App />}>
-            <Route index element={<Home />} />
-            <Route path="teams" element={<Teams />}>
-              <Route path=":teamId" element={<Team />} />
-              <Route path="new" element={<NewTeamForm />} />
-              <Route index element={<LeagueStandings />} />
-            </Route>
-          </Route>
-          <Route element={<PageLayout />}>
-            <Route path="/privacy" element={<Privacy />} />
-            <Route path="/tos" element={<Tos />} />
-          </Route>
-          <Route path="contact-us" element={<Contact />} />*/}
       </Routes>
     </BrowserRouter>
+    </GlobalTheme>
     </QueryClientProvider>
   );
 }
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,5 +1,6 @@
 import { useState,  } from 'react';
 import { useSearchStr} from '../lib/useSearchStr';
+import CustomRadio from './CustomRadio';
 
 const SearchForm = ({ onSearch }) =>{
     const [searchStr, setSearchStr] = useSearchStr();
@@ -27,15 +28,23 @@ const SearchForm = ({ onSearch }) =>{
     return(
           <form onSubmit={onSubmit}>
         <input type="text" value={searchStr} onChange={onSearchInputChange} />
-        <label>
-            Shows   
-            <input type="radio" name="search-option" value="shows" checked={searchOption ==='shows'} onChange={onRadioChange} /> 
-        </label>   
 
-        <label>
-        Actors 
-        <input type="radio" name="search-option" value="actors" checked={searchOption ==='actors'} onChange={onRadioChange}/>   
-        </label>    
+          <CustomRadio 
+            label="Shows"
+             name="search-option" 
+             value="shows" 
+             checked={searchOption ==='shows'} 
+             onChange={onRadioChange}
+            />
+            
+            <CustomRadio 
+            label="Actors"
+             name="search-option" 
+             value="actors" 
+             checked={searchOption ==='actors'} 
+             onChange={onRadioChange}
+            />
+            
         <button type="submit">Search</button>
   </form>
     )
